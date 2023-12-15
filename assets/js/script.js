f0d400f12ae81d2e1f7c24a98db994be681ce02c
LookforLuke <>
2023-12-15 03:39:35 +0000

    feat: Add event listeners for DOM Load and click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "play-again") {
+                alert("You chose play again");
+            } else {
+                let userChoice = this.getAttribute("data-type");
+                alert(`You selected ${userChoice}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
 
 function getcomputerselection() {
-    
+
 }
 
 function determineWinner() {
