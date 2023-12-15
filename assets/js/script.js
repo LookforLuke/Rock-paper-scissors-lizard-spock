1c41c57d58a34f3649cdde2c4c2118a1c84a1515
LookforLuke <>
2023-12-15 06:56:12 +0000

    maint: amend button data types and labels, revert event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.selection-area .btn--big').forEach(btn => {
-        btn.addEventListener('click', (ev) => {
-            userSelection = getUserSelection(ev.target);
-            console.log(userSelection);    
-            startGame();
-        } )
-    })
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "play-again") {
+                alert("You chose play again");
+            } else {
+                let userChoice = this.getAttribute("data-type");
+                alert(`You selected ${userChoice}`);
+            }
+        })
+    }
 })
 
-//code from https://torstencurdt.com/tech/posts/modulo-of-negative-numbers/
-function mod(a, b) {
-    c = a % b
-    return (c < 0) ? c + b : c
+function runGame() {
+
 }
-//code from https://torstencurdt.com/tech/posts/modulo-of-negative-numbers/
 
 /**
  * Called when generating computer selection
@@ -21,38 +23,22 @@ function mod(a, b) {
 function getComputerSelection() {
     
     //Creates a random number between 0 and 4
-    return actions [Math.floor(Math.random() * 5)];
+    let pcChoice = Math.floor(Math.random() * 5);
 
 }
 
-/**
- * called when retrieving user selection
- */
-function getUserSelection(target) {
-    console.log(target);
-    if (target.nodeName === 'i') {
-        return target.parentElement.classList[1];
-    }
-    return target.classList[1];
+function getUserSelection() {
+
 }
 
-function determineWinner(choice1, choice2) {
-    x = choices.indexOf(choice1);
-    y - choice1.indexOf(choice2);
-    if (x === y) {
-        return "Tie";
-    }
-    if (mod((x - y), choices.length) < choices.length / 2) {
-        return choice1 + "wins";
-    } else {
-        return choice2 + "wins";
-    }
+function determineWinner() {
+
 }
 
 function increaseScore() {
 
 }
 
-function displaySelections() {
+function displayResult() {
 
 }
